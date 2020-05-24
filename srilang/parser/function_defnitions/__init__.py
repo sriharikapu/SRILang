4a9c0a15bbf936c574b0a3cc077e1d252e7c2e7e
srihari kapu <sriharikapu163@gmail.com>
2020-05-24 23:42:02 +0800

    Create __init__.py

---
+++ srilang/parser/function_defnitions/__init__.py
@@ -0,0 +1,15 @@
+import sys
+
+from . import folding, nodes, validation  # noqa: F401
+from .nodes import compare_nodes  # noqa: F401
+from .utils import ast_to_dict, parse_to_ast  # noqa: F401
+
+from .natspec import parse_natspec  # noqa: F401; isort:skip
+
+# adds srilang.ast.nodes classes into the local namespace
+for name, obj in (
+    (k, v)
+    for k, v in nodes.__dict__.items()
+    if type(v) is type and nodes.srilangNode in v.__mro__
+):
+    setattr(sys.modules[__name__], name, obj)

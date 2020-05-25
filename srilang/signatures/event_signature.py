354e0589ffb5d60c8ea6f13062fbc6aec43af11c
srihari kapu <sriharikapu163@gmail.com>
2020-05-25 13:15:31 +0800

    Create event_signature.py

---
+++ srilang/signatures/event_signature.py
@@ -0,0 +1,118 @@
+from srilang import ast as sri_ast
+from srilang.exceptions import (
+    EventDeclarationException,
+    InvalidType,
+    VariableDeclarationException,
+)
+from srilang.signatures.function_signature import VariableRecord
+from srilang.types import ByteArrayType, canonicalize_type, get_size_of_type
+from srilang.utils import (
+    bytes_to_int,
+    ceil32,
+    check_valid_varname,
+    iterable_cast,
+    keccak256,
+)
+
+
+# Event signature object
+class EventSignature:
+    def __init__(self, name, args, indexed_list, event_id, sig):
+        self.name = name
+        self.args = args
+        self.indexed_list = indexed_list
+        self.sig = sig
+        self.event_id = event_id
+
+    # Get a signature from an event declaration
+    @classmethod
+    def from_declaration(cls, code, global_ctx):
+        name = code.target.id
+        pos = 0
+
+        check_valid_varname(
+            name, global_ctx._structs, global_ctx._constants,
+            pos=code, error_prefix="Event name invalid. ", exc=EventDeclarationException
+        )
+
+        # Determine the arguments, expects something of the form def foo(arg1: num, arg2: num ...
+        args = []
+        indexed_list = []
+        topics_count = 1
+        if code.annotation.args:
+            keys = code.annotation.args[0].keys
+            values = code.annotation.args[0].values
+            for i in range(len(keys)):
+                typ = values[i]
+                if not isinstance(keys[i], sri_ast.Name):
+                    raise EventDeclarationException(
+                        'Invalid key type, expected a valid name.',
+                        keys[i],
+                    )
+                if not isinstance(typ, (sri_ast.Name, sri_ast.Call, sri_ast.Subscript)):
+                    raise EventDeclarationException('Invalid event argument type.', typ)
+                if isinstance(typ, sri_ast.Call) and not isinstance(typ.func, sri_ast.Name):
+                    raise EventDeclarationException('Invalid event argument type', typ)
+                arg = keys[i].id
+                arg_item = keys[i]
+                is_indexed = False
+
+                # Check to see if argument is a topic
+                if isinstance(typ, sri_ast.Call) and typ.func.id == 'indexed':
+                    typ = values[i].args[0]
+                    indexed_list.append(True)
+                    topics_count += 1
+                    is_indexed = True
+                else:
+                    indexed_list.append(False)
+                if isinstance(typ, sri_ast.Subscript) and getattr(typ.value, 'id', None) == 'bytes' and typ.slice.value.n > 32 and is_indexed:  # noqa: E501
+                    raise EventDeclarationException("Indexed arguments are limited to 32 bytes")
+                if topics_count > 4:
+                    raise EventDeclarationException(
+                        f"Maximum of 3 topics {topics_count - 1} given",
+                        arg,
+                    )
+                if not isinstance(arg, str):
+                    raise VariableDeclarationException("Argument name invalid", arg)
+                if not typ:
+                    raise InvalidType("Argument must have type", arg)
+                check_valid_varname(
+                    arg,
+                    global_ctx._structs,
+                    global_ctx._constants,
+                    pos=arg_item,
+                    error_prefix="Event argument name invalid or reserved.",
+                )
+                if arg in (x.name for x in args):
+                    raise VariableDeclarationException(
+                        "Duplicate function argument name: " + arg,
+                        arg_item,
+                    )
+                # Can struct be logged?
+                parsed_type = global_ctx.parse_type(typ, None)
+                args.append(VariableRecord(arg, pos, parsed_type, False))
+                if isinstance(parsed_type, ByteArrayType):
+                    pos += ceil32(typ.slice.value.n)
+                else:
+                    pos += get_size_of_type(parsed_type) * 32
+        sig = name + '(' + ','.join([canonicalize_type(arg.typ, indexed_list[pos]) for pos, arg in enumerate(args)]) + ')'  # noqa F812
+        event_id = bytes_to_int(keccak256(bytes(sig, 'utf-8')))
+        return cls(name, args, indexed_list, event_id, sig)
+
+    @iterable_cast(dict)
+    def to_abi_event_dict(self, arg, pos):
+        yield "type", canonicalize_type(arg.typ, self.indexed_list[pos]),
+        yield "name", arg.name,
+        yield "indexed", self.indexed_list[pos],
+
+    def to_abi_dict(self):
+        abi_dict = {
+            "name": self.name,
+            "inputs": [
+                self.to_abi_event_dict(arg, pos)
+                for pos, arg in enumerate(self.args)
+            ] if self.args else [],
+            "anonymous": False,
+            "type": "event"
+        }
+        return abi_dict

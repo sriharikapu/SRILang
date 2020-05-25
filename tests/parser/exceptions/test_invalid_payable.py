ba10f3f08996666d23bb08d4daba8e59b8de902e
srihari kapu <sriharikapu163@gmail.com>
2020-05-25 14:56:51 +0800

    Create test_invalid_payable.py

---
+++ tests/parser/exceptions/test_invalid_payable.py
@@ -0,0 +1,42 @@
+import pytest
+from pytest import raises
+
+from srilang import compiler
+from srilang.exceptions import NonPayableViolation
+
+fail_list = [
+    """
+@public
+def foo():
+    x = msg.value
+"""
+]
+
+
+@pytest.mark.parametrize('bad_code', fail_list)
+def test_variable_decleration_exception(bad_code):
+    with raises(NonPayableViolation):
+        compiler.compile_code(bad_code)
+
+
+valid_list = [
+    """
+x: int128
+@public
+@payable
+def foo() -> int128:
+    self.x = 5
+    return self.x
+    """,
+    """
+@public
+@payable
+def foo():
+    x: uint256 = msg.value
+    """
+]
+
+
+@pytest.mark.parametrize('good_code', valid_list)
+def test_block_success(good_code):
+    assert compiler.compile_code(good_code) is not None
